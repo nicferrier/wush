32e48e794dba0658adb74a6785c8c3e9ca2357ed
Nic Ferrier <nic@ferrier.me.uk>
2018-11-08 23:52:24 +0000

    fs stuff

---
+++ basefs.js
@@ -0,0 +1,117 @@
+// A lot of this was nicked from https://www.stavros.io/posts/python-fuse-filesystem/
+const path = require("path");
+
+class Stat {
+}
+
+class FsStat {
+}
+
+class FsError {  // can this extend exception?
+}
+
+const FsErrors = {
+    EACCES: -1
+};
+
+class Fs {
+    constructor() {
+    }
+
+    access(path, mode) {
+        throw new FsError(FsErrors.EACCES);
+    }
+
+    chmod(path, mode) {
+    }
+
+    chown(path, uid, gid) {
+    }
+
+    getattr(path) {
+        // This is the Python
+        // return dict(
+        //  (key, getattr(st, key)) for key in (
+        //       'st_atime',
+        //       'st_ctime',
+        //       'st_gid',
+        //       'st_mode',
+        //       'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
+
+        return new Stat();
+    }
+
+    readdir(path) {
+    }
+
+    readlink(path) {
+    }
+
+    mknod(path, mode, dev) {
+    }
+
+    rmdir(path) {
+    }
+
+    mkdir(path, mode) {
+    }
+
+    statfs(path) {
+        // Python 
+        //  stv = os.statvfs(full_path)
+        //  return dict((key, getattr(stv, key)) for key in (
+        //    'f_bavail',
+        //    'f_bfree',
+        //    'f_blocks',
+        //    'f_bsize',
+        //    'f_favail',
+        //    'f_ffree',
+        //    'f_files',
+        //    'f_flag',
+        //    'f_frsize',
+        //    'f_namemax'))
+        return new FsStat();
+    }
+
+    unlink(path) {
+    }
+
+    symlink(name, target) {
+    }
+
+    rename(oldName, newName) {
+    }
+
+    link(target, name) {
+    }
+
+    utimens(path, times) { // time = None in the python
+    }
+
+    open(path, flags) {
+    }
+
+    create(path, mode, fi) {  // fi = None in the python
+    }
+
+    truncate(self, path, length, fh) { // fh = None in the python
+    }
+
+    flush(path, fh) {
+    }
+
+    release(path, fh) {
+    }
+
+    fsync(path, fdatasync, fh) {
+    }
+}
+
+
+module.exports = {
+    Stat,
+    FsStat,
+    FsError,
+    FsErrors,
+    Fs
+};
--- fsfs.js
+++ fsfs.js
@@ -1,124 +1,19 @@
-// A lot of this was nicked from https://www.stavros.io/posts/python-fuse-filesystem/
+const base = require("./basefs.js");
 const path = require("path");
 
-class Stat {
-}
+const fs = require("fs");
 
-class FsStat {
-}
-
-class FsError {  // can this extend exception?
-}
-
-const FsErrors = {
-    EACCES: -1
-};
-
-class Fs {
-    constructor() {
-    }
-
-    access(path, mode) {
-        throw new FsError(FsErrors.EACCES);
-    }
-
-    chmod(path, mode) {
-    }
-
-    chown(path, uid, gid) {
-    }
-
-    getattr(path) {
-        // This is the Python
-        // return dict(
-        //  (key, getattr(st, key)) for key in (
-        //       'st_atime',
-        //       'st_ctime',
-        //       'st_gid',
-        //       'st_mode',
-        //       'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
-
-        return new Stat();
-    }
-
-    readdir(path) {
-    }
-
-    readlink(path) {
-    }
-
-    mknod(path, mode, dev) {
-    }
-
-    rmdir(path) {
-    }
-
-    mkdir(path, mode) {
-    }
-
-    statfs(path) {
-        // Python 
-        //  stv = os.statvfs(full_path)
-        //  return dict((key, getattr(stv, key)) for key in (
-        //    'f_bavail',
-        //    'f_bfree',
-        //    'f_blocks',
-        //    'f_bsize',
-        //    'f_favail',
-        //    'f_ffree',
-        //    'f_files',
-        //    'f_flag',
-        //    'f_frsize',
-        //    'f_namemax'))
-        return new FsStat();
-    }
-
-    unlink(path) {
-    }
-
-    symlink(name, target) {
-    }
-
-    rename(oldName, newName) {
-    }
-
-    link(target, name) {
-    }
-
-    utimens(path, times) { // time = None in the python
-    }
-
-    open(path, flags) {
-    }
-
-    create(path, mode, fi) {  // fi = None in the python
-    }
-
-    truncate(self, path, length, fh) { // fh = None in the python
-    }
-
-    flush(path, fh) {
-    }
-
-    release(path, fh) {
-    }
-
-    fsync(path, fdatasync, fh) {
-    }
-}
-
-class FsFs extends Fs {
+class FsFs extends base.Fs {
     constructor(baseDir) {
         super();
         this.baseDir = baseDir;
     }
 
-    _join(path) {
-        return path.join(this.baseDir, path);
+    _join(pathToJoin) {
+        return path.join(this.baseDir, pathToJoin);
     }
 
     access(path, mode) {
-        
     }
 
     getattr(path) {
@@ -131,50 +26,86 @@ class FsFs extends Fs {
     }
 
     open(path, flags) {
+        console.log("fsfs path", path);
         return fs.createReadStream(this._join(path)); // FIXME no flags!
     }
 }
 
-const mounts = {
-    "/": new FsFs(path.join(__dirname, "testfs")),
-    "/home/nic": new FsFs("/home/nicferrier")
+class RedirectFsFs extends FsFs {
+    constructor(baseDir, mountDir) {
+        super(baseDir);
+        this.mountPoint = mountDir;
+    }
+
+    _join(pathToJoin) {
+        const [_, rebasedPath]
+              = new RegExp(this.mountPoint + "(/.*)")
+              .exec(pathToJoin);
+        console.log("rebasedPath", rebasedPath);
+        return path.join(this.baseDir, rebasedPath);
+    }
+
+    access(path, mode) {
+    }
+
+    getattr(path) {
+    }
+
+    readdir(path) {
+    }
+
+    readlink(path) {
+    }
+
+    open(path, flags) {
+        console.log("fsfs path", path);
+        return fs.createReadStream(this._join(path)); // FIXME no flags!
+    }
 }
 
-class TopFs extends Fs {
+const mounts = require("./mounts.js");
+
+// top-level virtual FS that deals with understanding mount points
+class VFs extends base.Fs {
     constructor(mounts) {
         super();
         this.mounts = mounts;
     }
 
     access(path, mode) {
+        const impl = mounts.match(path, this.mounts);
+        return impl.access(path, mode);
     }
 
     getattr(path) {
+        const impl = mounts.match(path, this.mounts);
+        return impl.getattr(path);
     }
 
     readdir(path) {
+        const impl = mounts.match(path, this.mounts);
+        return impl.readdir(path);
     }
 
     readlink(path) {
+        const impl = mounts.match(path, this.mounts);
+        return impl.readlink(path);
     }
 
     open(path, flags) {
-        const impl = this.mounts[path];
+        const impl = mounts.match(path, this.mounts);
         return impl.open(path, flags);
     }
 }
 
-function matchMount (path, mounts) {
-    const candidates = Object.keys(mounts).filter(mountPath => new RegExp("^" + mountPath).exec(path) );
-    const matched = candidates.slice().sort().reverse()[0];
-    return mounts[matched];
+const mountPoints = {
+    "/": new FsFs(path.join(__dirname, "testfs")),
+    "/home/nic": new RedirectFsFs("/home/nicferrier", "/home/nic")
 }
 
-console.log(matchMount("/home/nic/bin/mypackage/one/two", {
-    "/": 1,
-    "/home/nic/bin/mypackage": 10,
-    "/home/nic": 2,
-    "/home/dan": 3
-}));
+const testfs = new VFs(mountPoints);
+const stream = testfs.open("/home/nic/emacs");
+stream.pipe(process.stdout);
+
 
 // End
